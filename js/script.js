4e89e56a65a6910bdee5fd221ff5a9b9ec1acdc9
Joe Rogers <rogersj17@gmail.com>
2017-02-13 20:52:55 -0500

    Made files consistent with style guide.

--- js/script.js
+++ js/script.js
@@ -5,21 +5,21 @@ function submitFunc() {
   if(textValue === "")
   {
     document.getElementById("message").innerHTML = "Error: No data in name";
-    document.getElementById("error-message").style.visibility="visible";
+    document.getElementById("errorMessage").style.visibility="visible";
     console.log("Error: No data in name ");
     error = true;
   }
   if(!(document.getElementById('question').checked))
   {
     document.getElementById("message").innerHTML = "Error: wrong answer to check box";
-    document.getElementById("error-message").style.visibility="visible";
+    document.getElementById("errorMessage").style.visibility="visible";
     console.log("Error: wrong answer.");
     error = true;
   }
   if(!error)
   {
-    document.getElementById("error-message").style.visibility="hidden";
+    document.getElementById("errorMessage").style.visibility="hidden";
     document.getElementById("form").style.display='none';
-    document.getElementById("success-message").style.visibility = "visible";
+    document.getElementById("successMessage").style.visibility = "visible";
   }
 }
